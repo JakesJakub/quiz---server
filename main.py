799ea4c9658094ccc5a998d1d268a3f6114746af
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-04-20 19:26:00 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-radio.set_group(69)
+radio.set_group(150)
 
+def on_received_value(name, value):
+    pass
+radio.on_received_value(on_received_value)
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-radio.setGroup(69)
+radio.setGroup(150)
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+})
