0f4465a90acecfd9af46f5ecc2026666590799bb
Jakub Jakeš <91247920+JakesJakub@users.noreply.github.com>
2022-05-08 15:23:23 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+#Pokud hlasování neprobíhá, server odešle klientovi 0, pokud je však zmáčknuto tlačítko A, hlasování začne a server odešle 1.
+#Po přijetí čísla 1, klient povolí vybrání odpovědi. Ta může být změněna kolikrát je potřeba. Jakmile je zmáčknuté logo, momentálně vybraná odpověď se odešle na server.
+#Až server přijme odpověď, klientovi odešle "Přijato" a klient dá krátkou melodií vědět, že jeho odpověď byla přijata.
+#Pokud je na serveru zmáčknuto znovu tlačítko A, hlasování se zastaví a po zmáčknutí B se zobrazí výsledky.
+#Pokud je stisknutý PIN0 kód se zresetuje a server klientovi pošle číslo 2. Jakmile to je přijato, klient se resetuje také.
 radio.set_group(150)
-cislo = 0
-
-hlasy = [{"serials": control.device_serial_number(), "vote": cislo}]
-print(hlasy)
-
-znak = String.from_char_code(cislo + 65)
+odpoved = 0
 prijima = 0
+A = 0
+B = 0
+C = 0
+D = 0
+
+znak = String.from_char_code(odpoved + 65)
 
 def on_received_value(name, value):
+    global A, B, C, D
     if prijima == 1:
-        radio.send_number(control.device_serial_number())
+        if odpoved == 0:
+            A += 1
+        if odpoved == 1:
+            B += 1
+        if odpoved == 2:
+            C += 1
+        if odpoved == 1:
+            D += 1
 radio.on_received_value(on_received_value)
 
 def on_button_pressed_a():
@@ -23,3 +37,26 @@ def on_button_pressed_a():
     if prijima > 1:
         prijima -= 2
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+if prijima == 0:
+    radio.send_number(0)
+if prijima == 1:
+    radio.send_number(1)
+
+def on_received_value2(name, value):
+    if True:
+        radio.send_string("Přijato")
+radio.on_received_value(on_received_value2)
+
+def on_button_pressed_b():
+    print("Výsledky:")
+    print("A = " + A)
+    print("B = " + B)
+    print("C = " + C)
+    print("D = " + D)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_pin_pressed_p0():
+    radio.send_number(2)
+    control.reset()
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
--- main.ts
+++ main.ts
@@ -1,16 +1,35 @@
+// Pokud hlasování neprobíhá, server odešle klientovi 0, pokud je však zmáčknuto tlačítko A, hlasování začne a server odešle 1.
+// Po přijetí čísla 1, klient povolí vybrání odpovědi. Ta může být změněna kolikrát je potřeba. Jakmile je zmáčknuté logo, momentálně vybraná odpověď se odešle na server.
+// Až server přijme odpověď, klientovi odešle "Přijato" a klient dá krátkou melodií vědět, že jeho odpověď byla přijata.
+// Pokud je na serveru zmáčknuto znovu tlačítko A, hlasování se zastaví a po zmáčknutí B se zobrazí výsledky.
+// Pokud je stisknutý PIN0 kód se zresetuje a server klientovi pošle číslo 2. Jakmile to je přijato, klient se resetuje také.
 radio.setGroup(150)
-let cislo = 0
-let hlasy = [ {
-    "serials" : control.deviceSerialNumber(),
-    "vote" : cislo,
-}
-]
-console.log(hlasy)
-let znak = String.fromCharCode(cislo + 65)
+let odpoved = 0
 let prijima = 0
+let A = 0
+let B = 0
+let C = 0
+let D = 0
+let znak = String.fromCharCode(odpoved + 65)
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
     if (prijima == 1) {
-        radio.sendNumber(control.deviceSerialNumber())
+        if (odpoved == 0) {
+            A += 1
+        }
+        
+        if (odpoved == 1) {
+            B += 1
+        }
+        
+        if (odpoved == 2) {
+            C += 1
+        }
+        
+        if (odpoved == 1) {
+            D += 1
+        }
+        
     }
     
 })
@@ -27,4 +46,29 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
         prijima -= 2
     }
     
+})
+if (prijima == 0) {
+    radio.sendNumber(0)
+}
+
+if (prijima == 1) {
+    radio.sendNumber(1)
+}
+
+radio.onReceivedValue(function on_received_value2(name: string, value: number) {
+    if (true) {
+        radio.sendString("Přijato")
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    console.log("Výsledky:")
+    console.log("A = " + A)
+    console.log("B = " + B)
+    console.log("C = " + C)
+    console.log("D = " + D)
+})
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    radio.sendNumber(2)
+    control.reset()
 })
